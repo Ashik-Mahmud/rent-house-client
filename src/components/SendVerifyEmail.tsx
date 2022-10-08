960a1c9abd82bd84d4bc04d95df18048525fcfba
ashikmahmud <ashikmamud187@gmail.com>
2022-10-09 02:05:46 +0600

    added note text in the SendVerifyEmail component

--- src/components/SendVerifyEmail.tsx
+++ src/components/SendVerifyEmail.tsx
@@ -8,6 +8,11 @@ const SendVerifyEmail = ({ title, desc }: Props) => {
     <div className="flex items-center justify-center gap-4 flex-col py-10">
       <h3 className="text-4xl font-bold">{title}</h3>
       <p>{desc}</p>
+      <small>
+        <b> Note:</b> After creating your account we will sent you verification
+        email as well If your account creation time not more then one hour
+        please check your email and verify it
+      </small>
       <button className="btn btn-lg btn-success rounded-full">
         Send Verify Email
       </button>
