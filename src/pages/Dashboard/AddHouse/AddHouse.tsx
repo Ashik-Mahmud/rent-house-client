de15050ade33ba8c901fb1249996e9a2625e419c
ashikmahmud <ashikmamud187@gmail.com>
2022-10-09 23:04:22 +0600

    convert our add house form into the react-hook-form

--- src/pages/Dashboard/AddHouse/AddHouse.tsx
+++ src/pages/Dashboard/AddHouse/AddHouse.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import { BiBath, BiBed, BiMoney } from "react-icons/bi";
 import { BsAlignEnd, BsHouse, BsLink, BsPen } from "react-icons/bs";
 import SendVerifyEmail from "../../../components/SendVerifyEmail";
@@ -12,6 +13,19 @@ const AddHouse = (props: Props) => {
 
   const isVerify = updatedUser?.isVerified;
 
+  /* Handle Add House Form Submit */
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const handleAddHouseFormSubmit = handleSubmit((data) => {
+    console.log(data);
+    reset();
+  });
+
   return (
     <div className="p-5 my-5 bg-white rounded">
       <div className="flex items-center gap-2">
@@ -20,13 +34,14 @@ const AddHouse = (props: Props) => {
       </div>
       <div className="mt-5">
         {isVerify ? (
-          <form>
+          <form onSubmit={handleAddHouseFormSubmit}>
             <div className="flex flex-col md:flex-row gap-3">
               <HouseInput title="Put Your House Name" icon={<BsHouse />}>
                 <input
                   type="text"
                   className="form-control outline-none pl-4 w-full"
                   placeholder="Name"
+                  {...register("name", { required: true })}
                 />
               </HouseInput>
 
@@ -35,11 +50,15 @@ const AddHouse = (props: Props) => {
                   type="text"
                   className="form-control outline-none pl-4 w-full"
                   placeholder="Address"
+                  {...register("address", { required: true })}
                 />
               </HouseInput>
 
               <HouseInput title="Put Your Category" icon={<BsHouse />}>
-                <select className="outline-none  w-full pl-4 cursor-pointer text-sm">
+                <select
+                  className="outline-none  w-full pl-4 cursor-pointer text-sm"
+                  {...register("category", { required: true })}
+                >
                   <option value="Bungalow">Bungalow</option>
                   <option value="Duplex">Duplex</option>
                   <option value="Flat">Flat</option>
@@ -48,14 +67,20 @@ const AddHouse = (props: Props) => {
               </HouseInput>
 
               <HouseInput title="Select House Type" icon={<BsHouse />}>
-                <select className="form-control outline-none pl-4 w-full">
+                <select
+                  className="form-control outline-none pl-4 w-full"
+                  {...register("houseType", { required: true })}
+                >
                   <option value="Rent">Rent</option>
                   <option value="Sale">Sale</option>
                 </select>
               </HouseInput>
 
               <HouseInput title="Select House Use For" icon={<BsHouse />}>
-                <select className="form-control outline-none pl-4 w-full">
+                <select
+                  className="form-control outline-none pl-4 w-full"
+                  {...register("houseUseFor", { required: true })}
+                >
                   <option value="Residential">Residential</option>
                   <option value="Commercial">Commercial</option>
                 </select>
@@ -67,6 +92,7 @@ const AddHouse = (props: Props) => {
                   type="text"
                   className="form-control outline-none pl-4 w-full"
                   placeholder="Price"
+                  {...register("price", { required: true })}
                 />
               </HouseInput>
 
@@ -78,6 +104,7 @@ const AddHouse = (props: Props) => {
                   type="number"
                   className="form-control outline-none pl-4 w-full"
                   placeholder="bedrooms"
+                  {...register("bedrooms", { required: true })}
                 />
               </HouseInput>
 
@@ -86,6 +113,7 @@ const AddHouse = (props: Props) => {
                   type="number"
                   className="form-control outline-none pl-4 w-full"
                   placeholder="bathrooms"
+                  {...register("bathrooms", { required: true })}
                 />
               </HouseInput>
 
@@ -94,6 +122,7 @@ const AddHouse = (props: Props) => {
                   type="text"
                   className="form-control outline-none pl-4 w-full"
                   placeholder="District"
+                  {...register("district", { required: true })}
                 />
               </HouseInput>
 
@@ -102,17 +131,17 @@ const AddHouse = (props: Props) => {
                   type="text"
                   className="form-control outline-none pl-4 w-full"
                   placeholder="City"
+                  {...register("city", { required: true })}
                 />
               </HouseInput>
             </div>
             <HouseInput title="Put Your Description" icon={<BsPen />}>
               <textarea
-                name=""
-                id=""
                 cols={10}
                 rows={5}
                 placeholder="Description"
                 className="w-full outline-none"
+                {...register("description", { required: true })}
               ></textarea>
             </HouseInput>
             <HouseInput title="Put Your Google Map Link" icon={<BsLink />}>
@@ -120,40 +149,56 @@ const AddHouse = (props: Props) => {
                 type="url"
                 className="form-control outline-none pl-4 w-full"
                 placeholder="URL"
+                {...register("googleMapLink", { required: true })}
               />
             </HouseInput>
             {/* Start */}
             <div className="flex flex-col md:flex-row gap-3">
               <HouseInput title="IsBooked" icon={<BsHouse />}>
-                <select className="form-control outline-none pl-4 w-full">
+                <select
+                  className="form-control outline-none pl-4 w-full"
+                  {...register("isBooked", { required: true })}
+                >
                   <option value="">Select</option>
                   <option value="Yes">Yes</option>
                   <option value="No">No</option>
                 </select>
               </HouseInput>
               <HouseInput title="isBachelorRoom" icon={<BsHouse />}>
-                <select className="form-control outline-none pl-4 w-full">
+                <select
+                  className="form-control outline-none pl-4 w-full"
+                  {...register("isBechelorRoom", { required: true })}
+                >
                   <option value="">Select</option>
                   <option value="Yes">Yes</option>
                   <option value="No">No</option>
                 </select>
               </HouseInput>
               <HouseInput title="Allow Question" icon={<BsHouse />}>
-                <select className="form-control outline-none pl-4 w-full">
+                <select
+                  className="form-control outline-none pl-4 w-full"
+                  {...register("allowQuestion", { required: true })}
+                >
                   <option value="">Select</option>
                   <option value="Yes">Yes</option>
                   <option value="No">No</option>
                 </select>
               </HouseInput>
-              <HouseInput title="Allow Question" icon={<BsHouse />}>
-                <select className="form-control outline-none pl-4 w-full">
+              <HouseInput title="Is Available" icon={<BsHouse />}>
+                <select
+                  className="form-control outline-none pl-4 w-full"
+                  {...register("isAvailable", { required: true })}
+                >
                   <option value="">Select</option>
                   <option value="Yes">Yes</option>
                   <option value="No">No</option>
                 </select>
               </HouseInput>
               <HouseInput title="Allow Review" icon={<BsHouse />}>
-                <select className="form-control outline-none pl-4 w-full">
+                <select
+                  className="form-control outline-none pl-4 w-full"
+                  {...register("allowReview", { required: true })}
+                >
                   <option value="">Select</option>
                   <option value="Yes">Yes</option>
                   <option value="No">No</option>
@@ -167,6 +212,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasDrawingRoom"
+                    {...register("hasDrawingRoom")}
                   />{" "}
                   <label htmlFor="hasDrawingRoom" className="cursor-pointer">
                     hasDrawingRoom
@@ -177,6 +223,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasDinningRoom"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasDinningRoom" className="cursor-pointer">
                     hasDinningRoom
@@ -187,6 +234,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasKitchen"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasKitchen" className="cursor-pointer">
                     hasKitchen
@@ -197,6 +245,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasStore"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasStore" className="cursor-pointer">
                     hasStore
@@ -207,6 +256,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasServantRoom"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasServantRoom" className="cursor-pointer">
                     hasServantRoom
@@ -217,6 +267,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasSwimmingPool"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasSwimmingPool" className="cursor-pointer">
                     hasSwimmingPool
@@ -227,6 +278,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasGym"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasGym" className="cursor-pointer">
                     hasGym
@@ -237,6 +289,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasLawn"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasLawn" className="cursor-pointer">
                     hasLawn
@@ -247,6 +300,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasGarage"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasGarage" className="cursor-pointer">
                     hasGarage
@@ -257,6 +311,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasCarParking"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasCarParking" className="cursor-pointer">
                     hasCarParking
@@ -267,6 +322,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasLift"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasLift" className="cursor-pointer">
                     hasLift
@@ -277,6 +333,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasGenerator"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasGenerator" className="cursor-pointer">
                     hasGenerator
@@ -287,6 +344,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasSecurity"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasSecurity" className="cursor-pointer">
                     hasSecurity
@@ -297,6 +355,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasCCTV"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasCCTV" className="cursor-pointer">
                     hasCCTV
@@ -307,6 +366,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasInternet"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasInternet" className="cursor-pointer">
                     hasInternet
@@ -317,6 +377,7 @@ const AddHouse = (props: Props) => {
                     type="checkbox"
                     className="toggle toggle-sm  rounded-full"
                     id="hasGas"
+                    {...register("hasDinningRoom")}
                   />{" "}
                   <label htmlFor="hasGas" className="cursor-pointer">
                     hasGas
@@ -326,10 +387,19 @@ const AddHouse = (props: Props) => {
             </HouseInput>
             <div>
               <HouseInput title="Image">
-                <input type="file" name="preview-image" />
+                <input
+                  type="file"
+                  {...register("previewImage", { required: true })}
+                  accept="image/*"
+                />
               </HouseInput>
               <HouseInput title="Galleries">
-                <input type="file" name="gallery-image" multiple />
+                <input
+                  type="file"
+                  multiple
+                  {...register("galleryImage", { required: true })}
+                  accept="image/*"
+                />
               </HouseInput>
             </div>
             <div className="py-5 my-3 flex justify-end gap-4">
