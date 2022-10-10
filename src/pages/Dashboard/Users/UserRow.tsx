5d4aad95e0eb9787e13a9d4019afdc14435014ef
ashikmahmud <ashikmamud187@gmail.com>
2022-10-10 19:39:07 +0600

    create request for change status

--- src/pages/Dashboard/Users/UserRow.tsx
+++ src/pages/Dashboard/Users/UserRow.tsx
@@ -10,6 +10,8 @@ import {
 import { GiChessQueen } from "react-icons/gi";
 import { toast } from "react-toastify";
 import { AxiosUser } from "../../../api/Axios";
+import useAuth from "../../../hooks/useAuth";
+import { authUserInterface } from "../../../interfaces/UserInterface";
 
 type Props = {
   data: any;
@@ -20,13 +22,28 @@ type Props = {
 const UserRow = ({ data, ind, refetch }: Props) => {
   const [isEdit, setIsEdit] = useState<boolean>(false);
   const [changeRole, setChangeRole] = useState<string>("");
+  const [changeStatus, setChangeStatus] = useState<string>("");
+  const { updatedUser } = useAuth<authUserInterface | any>({});
+
   /* Handle Change Role */
   const handleChangeRole = async (id: string) => {
+    if (!changeRole) return toast.error("Select Role");
+    adminAction(changeRole, id, "/change-role");
+  };
+
+  /* Handle Change Status */
+  const handleStatus = async (id: string) => {
+    if (!changeStatus) return toast.error("Select Status");
+    adminAction(changeStatus, id, "/change-status");
+  };
+
+  /* Common function for changing */
+  const adminAction = async (changeText: string, id: string, url: string) => {
     const isConfirm = window.confirm("Are you sure?");
     try {
       if (isConfirm) {
-        const { data } = await AxiosUser.patch("/admin/change-role", {
-          role: changeRole,
+        const { data } = await AxiosUser.patch(`/admin${url}`, {
+          query: changeText,
           id: id,
         });
         toast.success(data?.message);
@@ -40,7 +57,17 @@ const UserRow = ({ data, ind, refetch }: Props) => {
 
   return (
     <tr>
-      <td className="text-left">{data?.name || "not available"}</td>
+      <td className="text-left">
+        {data?.name || "not available"}{" "}
+        {data?._id === updatedUser?._id && (
+          <span
+            className="text-green-400 text-xl cursor-pointer tooltip tooltip-success"
+            data-tip="Active User"
+          >
+            *
+          </span>
+        )}
+      </td>
       <td className="text-left">
         <span className="text-gray-500">{data?.email || "not available"}</span>
       </td>
@@ -96,6 +123,7 @@ const UserRow = ({ data, ind, refetch }: Props) => {
               className="outline-none cursor-pointer  rounded-md w-24"
               onChange={(e) => setChangeRole(e.target.value)}
             >
+              <option value="">select</option>
               <option value="customer">Customer</option>
               <option value="user">User - House Holder</option>
               <option value="admin">Admin</option>
@@ -140,14 +168,17 @@ const UserRow = ({ data, ind, refetch }: Props) => {
               name=""
               id=""
               className="outline-none cursor-pointer  rounded-md w-24"
+              onChange={(e) => setChangeStatus(e.target.value)}
             >
-              <option value="">Active</option>
-              <option value="">Inactive</option>
+              <option value="">Select</option>
+              <option value="active">Active</option>
+              <option value="inactive">Inactive</option>
             </select>
             <div className=" select-none flex items-center gap-1">
               <span
                 className="text-success cursor-pointer text-2xl tooltip"
                 data-tip="Save Changes"
+                onClick={() => handleStatus(data?._id)}
               >
                 <BsCheck2 />
               </span>
@@ -173,20 +204,31 @@ const UserRow = ({ data, ind, refetch }: Props) => {
       <td className="text-left">
         <div className="flex items-center gap-3">
           {!isEdit ? (
-            <button className="" onClick={() => setIsEdit(true)}>
+            <button
+              className={`${
+                data?._id === updatedUser?._id &&
+                "pointer-events-none text-gray-300"
+              }`}
+              onClick={() => setIsEdit(true)}
+            >
               <span className="w-5 h-5 flex items-center justify-center">
                 <BsPen />
               </span>
             </button>
           ) : (
-            <button className="" onClick={() => setIsEdit(false)}>
+            <button onClick={() => setIsEdit(false)}>
               <span className="text-2xl flex items-center justify-center">
                 &times;
               </span>
             </button>
           )}
 
-          <button className="">
+          <button
+            className={`${
+              data?._id === updatedUser?._id &&
+              "pointer-events-none text-gray-300"
+            }`}
+          >
             <span className="w-5 h-5 flex items-center justify-center">
               <BsTrash />
             </span>
