991f4d730d223d52a7d1a837361535ad3bd45674
ashikmahmud <ashikmamud187@gmail.com>
2022-10-12 19:23:52 +0600

    add modal for requesting blog

--- src/pages/Dashboard/Settings/VerifyBlogModal.tsx
+++ src/pages/Dashboard/Settings/VerifyBlogModal.tsx
@@ -1,3 +1,5 @@
+import { BiLink, BiPen } from "react-icons/bi";
+
 type Props = {};
 
 const VerifyBlogModal = (props: Props) => {
@@ -13,12 +15,58 @@ const VerifyBlogModal = (props: Props) => {
             ✕
           </label>
           <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
+            Add Notes Why you access blog option?
           </h3>
           <p className="py-4">
             You've been selected for a chance to get one year of subscription to
             use Wikipedia for free!
           </p>
+          <form action="">
+            {/* url */}
+            <div className="name border  rounded p-3 relative mt-10 flex-1">
+              <div className="name-title absolute -top-4 bg-white border rounded p-1">
+                <h3 className="text-xs font-poppins">
+                  Put Your Best Blog Link
+                </h3>
+              </div>
+              <div className="input-group flex items-center my-2 border p-3 rounded-md mt-2">
+                <div className="icon">
+                  <BiLink />
+                </div>
+                <input
+                  type="url"
+                  className="form-control outline-none pl-4 w-full"
+                  placeholder="Blog URL"
+                />
+              </div>
+            </div>
+            {/* End */}
+            {/* url */}
+            <div className="name border  rounded p-3 relative mt-10 flex-1">
+              <div className="name-title absolute -top-4 bg-white border rounded p-1">
+                <h3 className="text-xs font-poppins">
+                  Why You want blog writing authority?
+                </h3>
+              </div>
+              <div className="input-group flex items-center my-2 border p-3 rounded-md mt-2">
+                <div className="icon">
+                  <BiPen />
+                </div>
+                <textarea
+                  name=""
+                  id=""
+                  cols={5}
+                  rows={4}
+                  className="w-full font-poppins text-md textarea"
+                  placeholder="Write why you need blog option?"
+                ></textarea>
+              </div>
+            </div>
+            {/* End */}
+            <div className="my-5 ">
+              <button className="btn btn-success mr-3">Send Request</button>
+            </div>
+          </form>
         </div>
       </div>
     </>
