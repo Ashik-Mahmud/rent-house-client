b0aeb8088fab80b4f3d4dc86f11ae5a2b18755af
ashikmahmud <ashikmamud187@gmail.com>
2022-10-12 19:08:18 +0600

    put modal for sending blog request

--- src/pages/Dashboard/Settings/Settings.tsx
+++ src/pages/Dashboard/Settings/Settings.tsx
@@ -8,6 +8,7 @@ import { logout } from "../../../features/AuthSlice";
 import useAuth from "../../../hooks/useAuth";
 import { authUserInterface } from "../../../interfaces/UserInterface";
 import { useChangePasswordMutation } from "../../../services/AuthApi";
+import VerifyBlogModal from "./VerifyBlogModal";
 
 type Props = {};
 
@@ -60,100 +61,108 @@ const Settings = (props: Props) => {
     }
   }, [isSuccess, data, error, dispatch, setUser, navigate]);
 
+  /* Request For Blog */
+
   return (
-    <div>
-      <div className="p-5 my-4 bg-white font-poppins">
-        <h3 className="text-2xl font-bold mb-3">Settings</h3>
+    <>
+      <VerifyBlogModal />
+      <div>
+        <div className="p-5 my-4 bg-white font-poppins">
+          <h3 className="text-2xl font-bold mb-3">Settings</h3>
 
-        <div className="settings-content">
-          {role !== "admin" && (
-            <>
-              {!isBlogAllowed && (
-                <div className="req-for-blog flex-col justify-center sm:justify-between sm:flex-row flex items-center my-8 bg-base-200 p-5 rounded-lg">
-                  <h3 className="text-xl font-bold text-center sm:text-left">
-                    If you want to get Blogs writing authority
-                  </h3>
+          <div className="settings-content">
+            {role !== "admin" && (
+              <>
+                {!isBlogAllowed && (
+                  <div className="req-for-blog flex-col justify-center sm:justify-between sm:flex-row flex items-center my-8 bg-base-200 p-5 rounded-lg">
+                    <h3 className="text-xl font-bold text-center sm:text-left">
+                      If you want to get Blogs writing authority
+                    </h3>
 
-                  {isVerify ? (
-                    <button className="btn btn-success rounded-full mt-4  sm:mt-0">
-                      Request For Blog
-                    </button>
-                  ) : (
-                    <button
-                      className="btn btn-warning pointer-events-none rounded-full mt-4  sm:mt-0 tooltip"
-                      data-tip="Verify First"
-                    >
-                      Verify Account to Get Request
-                    </button>
-                  )}
-                </div>
-              )}
-              {role === "customer" && (
-                <div className="req-for-blog flex items-center  my-8 bg-base-200 p-5 rounded-lg flex-col justify-center sm:justify-between sm:flex-row">
-                  <h3 className="text-xl font-bold text-center sm:text-left">
-                    If you want to get House Holder Account
-                  </h3>
-                  {isVerify ? (
-                    <button className="btn btn-success rounded-full mt-4 sm:mt-0">
-                      Request For House Holder Account
-                    </button>
-                  ) : (
-                    <button
-                      className="btn btn-warning pointer-events-none rounded-full mt-4  sm:mt-0 tooltip"
-                      data-tip="Verify First"
-                    >
-                      Verify Account to Get Request
-                    </button>
-                  )}
-                </div>
-              )}
-            </>
-          )}
+                    {isVerify ? (
+                      <label
+                        htmlFor="my-modal-for-blog"
+                        className="btn btn-success rounded-full mt-4  sm:mt-0"
+                      >
+                        Request For Blog
+                      </label>
+                    ) : (
+                      <button
+                        className="btn btn-warning pointer-events-none rounded-full mt-4  sm:mt-0 tooltip"
+                        data-tip="Verify First"
+                      >
+                        Verify Account to Get Request
+                      </button>
+                    )}
+                  </div>
+                )}
+                {role === "customer" && (
+                  <div className="req-for-blog flex items-center  my-8 bg-base-200 p-5 rounded-lg flex-col justify-center sm:justify-between sm:flex-row">
+                    <h3 className="text-xl font-bold text-center sm:text-left">
+                      If you want to get House Holder Account
+                    </h3>
+                    {isVerify ? (
+                      <button className="btn btn-success rounded-full mt-4 sm:mt-0">
+                        Request For House Holder Account
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-warning pointer-events-none rounded-full mt-4  sm:mt-0 tooltip"
+                        data-tip="Verify First"
+                      >
+                        Verify Account to Get Request
+                      </button>
+                    )}
+                  </div>
+                )}
+              </>
+            )}
 
-          <div className="password-change-field bg-gray-50 p-8 rounded">
-            <h3 className="text-2xl font-bold mb-6">Change Password</h3>
-            <form action="" onSubmit={handleChangePassword}>
-              <div className="input-group flex items-center my-1 border p-3 rounded-md mt-2 bg-white">
-                <div className="icon">
-                  <BiLockAlt />
+            <div className="password-change-field bg-gray-50 p-8 rounded">
+              <h3 className="text-2xl font-bold mb-6">Change Password</h3>
+              <form action="" onSubmit={handleChangePassword}>
+                <div className="input-group flex items-center my-1 border p-3 rounded-md mt-2 bg-white">
+                  <div className="icon">
+                    <BiLockAlt />
+                  </div>
+                  <input
+                    type="password"
+                    className="outline-none  w-full pl-4 text-sm"
+                    placeholder="Old Password"
+                    {...register("oldPassword")}
+                  />
                 </div>
-                <input
-                  type="password"
-                  className="outline-none  w-full pl-4 text-sm"
-                  placeholder="Old Password"
-                  {...register("oldPassword")}
-                />
-              </div>
-              <div className="input-group flex items-center my-1 border p-3 rounded-md mt-2 bg-white">
-                <div className="icon">
-                  <BiLockAlt />
+                <div className="input-group flex items-center my-1 border p-3 rounded-md mt-2 bg-white">
+                  <div className="icon">
+                    <BiLockAlt />
+                  </div>
+                  <input
+                    type="password"
+                    className="outline-none  w-full pl-4  text-sm"
+                    placeholder="New Password"
+                    {...register("newPassword")}
+                  />
                 </div>
-                <input
-                  type="password"
-                  className="outline-none  w-full pl-4  text-sm"
-                  placeholder="New Password"
-                  {...register("newPassword")}
-                />
-              </div>
-              <div className="input-group flex items-center my-1 border p-3 rounded-md mt-2 bg-white">
-                <div className="icon">
-                  <BiLockAlt />
+                <div className="input-group flex items-center my-1 border p-3 rounded-md mt-2 bg-white">
+                  <div className="icon">
+                    <BiLockAlt />
+                  </div>
+                  <input
+                    type="password"
+                    className="outline-none  w-full pl-4  text-sm"
+                    placeholder="Confirm Password"
+                    {...register("repeatNewPassword")}
+                  />
                 </div>
-                <input
-                  type="password"
-                  className="outline-none  w-full pl-4  text-sm"
-                  placeholder="Confirm Password"
-                  {...register("repeatNewPassword")}
-                />
-              </div>
-              <button className="btn btn-success rounded-full mt-5">
-                Change Password
-              </button>
-            </form>
+                <button className="btn btn-success rounded-full mt-5">
+                  Change Password
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
