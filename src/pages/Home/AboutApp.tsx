21324b66d46348b0cdb878a64fed3b4b50288b05
ashikmahmud <ashikmamud187@gmail.com>
2022-10-14 00:13:01 +0600

    added search filter in hero section

---
+++ src/pages/Home/AboutApp.tsx
@@ -0,0 +1,18 @@
+type Props = {};
+
+const AboutApp = (props: Props) => {
+  return (
+    <div>
+      <div className="container mx-auto">
+        <h1 className="text-4xl font-semibold mb-4 text-center font-open m-16 ipad:m-20">
+          About App
+        </h1>
+        <div>
+          <h3>Section Coming soon...</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutApp;
