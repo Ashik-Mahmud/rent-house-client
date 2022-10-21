6aaa2e84df994836c0e28b9326f1ccd3f3c048ca
ashikmahmud <ashikmamud187@gmail.com>
2022-10-21 18:10:19 +0600

    fixes issues and sendEmailVerification func for send verification email again

--- src/components/SendVerifyEmail.tsx
+++ src/components/SendVerifyEmail.tsx
@@ -1,9 +1,40 @@
+import axios from "axios";
+import swal from "sweetalert";
+import useAuth from "../hooks/useAuth";
+import { authUserInterface } from "../interfaces/UserInterface";
+
 type Props = {
   title: string;
   desc: string;
 };
 
 const SendVerifyEmail = ({ title, desc }: Props) => {
+  const { updatedUser, user } = useAuth<authUserInterface | any>({});
+  /* Handle Verification Email */
+  const handleVerificationEmail = async () => {
+    const isConfirm = await swal({
+      title: `Please Confirm `,
+      text: `We will sent you again verification email please check email to ${updatedUser?.email}`,
+      buttons: ["cancel", "Confirm"],
+    });
+    if (isConfirm) {
+      const { data } = await axios.get(
+        `http://localhost:5000/api/v1/users/send-verification-email/${updatedUser?._id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${user?.token}`,
+          },
+        }
+      );
+      await swal({
+        title: `${data?.message}`,
+        text: "Please check we will sent you again verification mail",
+        icon: "success",
+        buttons: ["cancel", "okay"],
+      });
+    }
+  };
+
   return (
     <div className="flex items-center justify-center gap-4 flex-col py-10">
       <h3 className="text-4xl font-bold">{title}</h3>
@@ -13,7 +44,10 @@ const SendVerifyEmail = ({ title, desc }: Props) => {
         email as well If your account creation time not more then one hour
         please check your email and verify it
       </small>
-      <button className="btn btn-lg btn-success rounded-full">
+      <button
+        className="btn btn-lg btn-success rounded-full"
+        onClick={handleVerificationEmail}
+      >
         Send Verify Email
       </button>
     </div>
