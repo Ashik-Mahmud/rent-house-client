e7c902514b4c3291ad25cc76a906d6a39ee1605f
ashikmahmud <ashikmamud187@gmail.com>
2022-10-21 22:28:33 +0600

    optimze icon and putting another state to the admin dashbaord for pending house

--- src/pages/Dashboard/Dashboard/AdminDashboard/AdminDashboard.tsx
+++ src/pages/Dashboard/Dashboard/AdminDashboard/AdminDashboard.tsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { BiUserCheck } from "react-icons/bi";
+import { BsBook, BsHouse, BsHouseDoor, BsHouseDoorFill } from "react-icons/bs";
 import { useQuery } from "react-query";
 import { useAppSelector } from "../../../../app/store";
 import GlobalLoader from "../../../../components/GlobalLoader";
@@ -12,9 +14,8 @@ import UsersCharts from "./UsersCharts";
 type Props = {};
 
 const AdminDashboard = (props: Props) => {
-  const { approvedHouseCount, rejectedHouseCount } = useAppSelector(
-    (state) => state.housesReqCount
-  );
+  const { approvedHouseCount, rejectedHouseCount, pendingHouseCount } =
+    useAppSelector((state) => state.housesReqCount);
   const { data: blogs, isLoading: loading2 } = useGetAllBlogsQuery({} as any);
 
   const { user, updatedUser } = useAuth<authUserInterface | any>({});
@@ -41,60 +42,32 @@ const AdminDashboard = (props: Props) => {
       <div className="stats gap-4 shadow flex justify-between">
         <div className="stat ">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
+            <BsHouse className="text-3xl text-success" />
           </div>
-          <div className="stat-title">Total Approved Houses</div>
-          <div className="stat-value">{approvedHouseCount}</div>
+          <div className="stat-title">Approved Houses</div>
+          <div className="stat-value text-success">{approvedHouseCount}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+        <div className="stat ">
+          <div className="stat-figure text-secondary">
+            <BsHouseDoorFill className="text-3xl text-info" />
+          </div>
+          <div className="stat-title">Unapproved Houses</div>
+          <div className="stat-value text-info">{pendingHouseCount}</div>
           <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
 
         <div className="stat">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-              ></path>
-            </svg>
+            <BsHouseDoor className="text-3xl text-error" />
           </div>
           <div className="stat-title">Total Rejected Houses</div>
-          <div className="stat-value">{rejectedHouseCount}</div>
+          <div className="stat-value text-error">{rejectedHouseCount}</div>
           <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
         <div className="stat">
           <div className="stat-figure text-primary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              ></path>
-            </svg>
+            <BsBook className="text-3xl text-primary" />
           </div>
           <div className="stat-title">Total Blogs</div>
           <div className="stat-value text-primary">{blogs?.count}</div>
@@ -103,19 +76,7 @@ const AdminDashboard = (props: Props) => {
 
         <div className="stat">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-              ></path>
-            </svg>
+            <BiUserCheck className="text-3xl text-primary" />
           </div>
           <div className="stat-title">Total Register Users</div>
           <div className="stat-value">{data?.count}</div>
