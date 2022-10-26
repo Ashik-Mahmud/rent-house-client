89a759b38e8a2314469715ac78f9775c195f7a31
ashikmahmud <ashikmamud187@gmail.com>
2022-10-26 22:28:51 +0600

    redesign home best houses card

--- src/pages/Home/BestHouse/BestHouses.tsx
+++ src/pages/Home/BestHouse/BestHouses.tsx
@@ -1,3 +1,5 @@
+import { BiStar } from "react-icons/bi";
+import { Link } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTItle";
 
 type Props = {};
@@ -15,98 +17,40 @@ const BestHouses = (props: Props) => {
       </div>
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          <div className="card">
-            <div className="card-body">
-              <div className="card-image">
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWw_fKQ6H8mfIq0v-fvyUiJL0osaQODoC2og&usqp=CAU"
-                  alt=""
-                  className="h-60 w-full object-cover"
-                />
+          {/* card */}
+          <div className="shadow p-5 rounded cursor-pointer">
+            <img
+              className="rounded object-cover w-full mb-4"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWw_fKQ6H8mfIq0v-fvyUiJL0osaQODoC2og&usqp=CAU"
+              alt=""
+            />
+            <div className="title">
+              <div className="flex items-center justify-between">
+                <h3 className="text-xl font-bold">This is product title</h3>
+                <span className="bg-gray-200 p-1 rounded-md text-xs">
+                  brand
+                </span>
               </div>
-              <div className="card-title">
-                <h1 className="text-2xl font-bold">House 1</h1>
-              </div>
-              <div className="card-subtitle">
-                <p className="text-gray-500">Location: Dhaka</p>
-              </div>
-              <div className="card-content">
-                <p className="text-gray-500">Price: 1,000,000</p>
-              </div>
-              <div className="card-actions">
-                <button className="btn btn-success">View Details</button>
+              <div className="flex items-center justify-between">
+                <span className="text-green-500">100 $</span>
+                <span className="text-xs text-sky-400">available</span>
               </div>
             </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <div className="card-image">
-                <img
-                  src="https://i.pinimg.com/originals/66/d9/f5/66d9f5afdc5337d3f9eac362b970c426.jpg"
-                  alt=""
-                  className="h-60 w-full object-cover"
-                />
-              </div>
-              <div className="card-title">
-                <h1 className="text-2xl font-bold">House 2</h1>
-              </div>
-              <div className="card-subtitle">
-                <p className="text-gray-500">Location: Dhaka</p>
-              </div>
-              <div className="card-content">
-                <p className="text-gray-500">Price: 1,000,000</p>
-              </div>
-              <div className="card-actions">
-                <button className="btn btn-success">View Details</button>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <div className="card-image">
-                <img
-                  src="https://www.bhg.com/thmb/SLX8yV-ipXwrrPqUgkeAalYr8zU=/1707x1280/smart/filters:no_upscale():focal(899x639:901x641)/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg"
-                  className="h-60 w-full object-cover"
-                  alt=""
-                />
-              </div>
-              <div className="card-title">
-                <h1 className="text-2xl font-bold">House 3</h1>
-              </div>
-              <div className="card-subtitle">
-                <p className="text-gray-500">Location: Dhaka</p>
-              </div>
-
-              <div className="card-content">
-                <p className="text-gray-500">Price: 1,000,000</p>
-              </div>
-              <div className="card-actions">
-                <button className="btn btn-success">View Details</button>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <div className="card-image">
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPJWlJdbbI_9cbPTPWgome8qc4m5nUmJohGw&usqp=CAU"
-                  alt=""
-                  className="h-60 w-full object-cover"
-                />
-              </div>
-              <div className="card-title">
-                <h1 className="text-2xl font-bold">House 4</h1>
-              </div>
-              <div className="card-subtitle">
-                <p className="text-gray-500">Location: Dhaka</p>
-              </div>
-
-              <div className="card-content">
-                <p className="text-gray-500">Price: 1,000,000</p>
-              </div>
-              <div className="card-actions">
-                <button className="btn btn-success">View Details</button>
-              </div>
+            <div className="info">
+              <span className="category">Duplex</span>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Distinctio, est.
+              </p>
+              <Link to={`/products/`}>
+                <span className="p-2 rounded bg-sky-100 mt-4 px-4 flex items-center justify-between">
+                  {" "}
+                  View Details
+                  <span title="Best Ratings">
+                    <BiStar />
+                  </span>
+                </span>
+              </Link>
             </div>
           </div>
         </div>
