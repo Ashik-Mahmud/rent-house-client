77e40281cc978bb356987f7ec04ab9296f0c1aa1
ashikmahmud <ashikmamud187@gmail.com>
2022-10-30 03:30:04 +0600

    done to fixes placeholder loader

--- src/pages/AppReviews/Reviews.tsx
+++ src/pages/AppReviews/Reviews.tsx
@@ -88,7 +88,7 @@ const Reviews = (props: Props) => {
 
         {isLoading ? (
           <div className="review-content grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-            {Array(searchedData?.length)
+            {Array(searchedData?.length || limit)
               .fill(0)
               .map((_, ind) => (
                 <ReviewSkeletonLoading key={ind} />
