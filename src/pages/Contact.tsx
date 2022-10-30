5ed12e5638703a2d6e0124b44605bf5dce3d96ca
ashikmahmud <ashikmamud187@gmail.com>
2022-10-30 23:06:51 +0600

    changes and putting real text

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -220,7 +220,7 @@ const Contact = (props: Props) => {
                   </div>
                   <div className="p-2 w-full">
                     <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                      Button
+                      Send Message
                     </button>
                   </div>
                   <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
