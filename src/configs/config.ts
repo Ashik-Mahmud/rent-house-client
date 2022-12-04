d2d0c93c8a0f56ef9943fad6ce6a88ac89af7057
ashikmahmud <ashikmamud187@gmail.com>
2022-12-04 22:32:48 +0600

    deploy link with base_backend_url

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,1 +1,2 @@
-export const base_backend_url = "https://rent-house-server.onrender.com";
+export const base_backend_url = "https://rent-house-server-hxau.onrender.com";
+// export const base_backend_url = "https://rent-house-server.onrender.com";
